590209aea2d98b3d2827dcabafee50f62922514a
Alvin Voo <xoalvinox@gmail.com>
2019-07-09 18:05:33 +0800

    refactor to use ProjectInvitationRequest

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -5,21 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Project;
 use App\User;
+use App\Http\Requests\ProjectInvitationRequest;
 
 class ProjectInvitationsController extends Controller
 {
     //
-    public function store(Project $project){
+    public function store(ProjectInvitationRequest $request, Project $project){
 
-        $this->authorize('update', $project);
+        // $this->authorize('update', $project);
 
-        $email = request()->validate([
-            'email' => 'required | exists:users,email'
-        ],[
-            'email.exists' => 'The user you are inviting must have a birdboard account.'
-        ]);
+        // $email = request()->validate([
+        //     'email' => 'required | exists:users,email'
+        // ],[
+        //     'email.exists' => 'The user you are inviting must have a birdboard account.'
+        // ]);
 
-        $user = User::whereEmail($email)->first();
+        $user = User::whereEmail(request('email'))->first();
 
         $project->invite($user);
 
